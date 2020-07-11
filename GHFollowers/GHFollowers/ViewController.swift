7cb499b085d4b365480cff1c4d33985663c6abf7
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-07-11 18:01:52 -0300

    setup project

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Luan Fagundes on 11/07/20.
+//  Copyright © 2020 Luan Fagundes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
