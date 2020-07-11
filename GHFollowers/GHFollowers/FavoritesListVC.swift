dc93ecb52ba6e87a7b7fc0510e6003877fbc901b
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-07-11 18:48:03 -0300

    navigation structure

--- GHFollowers/GHFollowers/FavoritesListVC.swift
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
 //  Created by Luan Fagundes on 11/07/20.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Luan Fagundes on 11/07/20.
+//  Copyright © 2020 Luan Fagundes. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGray
+    }
+}
