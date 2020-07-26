91b874e2bc13f038f63ab06fbdace8d92bf69ecc
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-07-26 00:39:46 -0300

    custom alert

---
+++ GHFollowers/GHFollowers/GFBodyLabel.swift
@@ -0,0 +1,34 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by Luan Fagundes on 25/07/20.
+//  Copyright © 2020 Luan Fagundes. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
