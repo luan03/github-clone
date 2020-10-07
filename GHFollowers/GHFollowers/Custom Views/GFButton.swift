c4db65fa5124436771a2763ff474419846895604
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-10-07 13:39:48 -0300

    fix title color

--- GHFollowers/GHFollowers/Custom Views/GFButton.swift
+++ GHFollowers/GHFollowers/Custom Views/GFButton.swift
@@ -29,7 +29,7 @@ class GFButton: UIButton {
     
     private func configure() {
         layer.cornerRadius = 10
-        titleLabel?.textColor = .white
+        setTitleColor(.white, for: .normal)
         // this adapts the font configuration on device
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
