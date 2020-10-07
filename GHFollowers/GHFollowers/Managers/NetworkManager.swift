c3169c8efce5f221bad17947526e1d45b2aac00d
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-10-07 19:00:49 -0300

    refactoring error message

--- GHFollowers/GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/GHFollowers/Managers/NetworkManager.swift
@@ -16,28 +16,28 @@ class NetworkManager {
 
     }
 
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void ) {
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void ) {
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
 
         // now we convert the string to an object URL in swift
         guard let url = URL(string: endpoint) else {
-            completed(nil, "This username created an invalid request. Please Try Again.")
+            completed(nil,  .invalidUsername)
             return
         }
 
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
 
             if let _  = error {
-                completed(nil, "Unable to complete your request. Please check your connection.")
+                completed(nil, .unableToComplete)
             }
 
             guard let response  = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "Invalid response from server. Please try again.")
+                completed(nil, .invalidResponse)
                 return
             }
 
             guard let data = data else {
-                completed(nil, "The Data received from server was invalid. Please Try Again")
+                completed(nil, .invalidData)
                 return
             }
 
@@ -47,7 +47,7 @@ class NetworkManager {
                 let followers =  try decoder.decode([Follower].self, from: data)
                 completed(followers, nil)
             } catch {
-                completed(nil, "The Data received from server was invalid. Please Try Again")
+                completed(nil, .invalidData)
             }
         }
 
---
+++ GHFollowers/GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Luan Fagundes on 07/10/20.
+//  Copyright © 2020 Luan Fagundes. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please Try Again."
+    case unableToComplete = "Unable to complete your request. Please check your connection."
+    case invalidResponse = "Invalid response from server. Please try again."
+    case invalidData = "The Data received from server was invalid. Please Try Again"
+}
